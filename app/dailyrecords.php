9af40802bfb09ac5e281c2d6abe28af24ab600b1
Frederick <mimawmimaw01@gmail.com>
2020-09-24 09:59:28 -0400

    edited CRUD

---
+++ app/dailyrecords.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class dailyrecords extends Model
+{
+    protected $guarded = [];
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Display all reservations
+Route::get('/dailyrecords', 'DailyrecordsController@index');
+
+// Display form to create reservation
+Route::get('/dailyrecords/create', 'DailyrecordsController@create');
+
+// Display specific reservation
+Route::get('/dailyrecords/{dailyrecord}', 'DailyrecordsController@show');
+
+// Store a Reservation
+Route::post('/dailyrecords', 'DailyrecordsController@store');
+
+// Display form to update reservation
+Route::get('/dailyrecords/{dailyrecord}/edit', 'DailyrecordsController@edit');
+
+// Update a Reservation
+Route::put('/dailyrecords/{dailyrecord}', 'DailyrecordsController@update');
+
+// Delete a reservation
+Route::delete('/dailyrecords/{dailyrecord}/delete', 'DailyrecordsController@delete'); 
