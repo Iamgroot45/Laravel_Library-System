f90006d0c691e71d1997329f75bead7f4efba59b
AllysaMaePalawag <allysamaepalawag@gmail.com>
2020-10-09 21:24:34 +0800

    eloquent model and database seeding

--- app/Borrower.php
+++ app/Borrower.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Borrower extends Model
 {
+    //eloquent model
     protected $guarded = [];
     public $timestamps = false;
 
---
+++ database/seeds/BorrowerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Borrower;
+class BorrowerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $borrower = Borrower::create([
+        	'last_name' => 'Lastname',
+        	'first_name' => 'Firstname',
+        	'middle_initial' => 'M',
+        	'contact_number' => '09090909099',
+        	'email_address' => 'email@seed.com'    
+        ]);
+    }
+}
