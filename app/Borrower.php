ae7941074a7a5e5fce393f866bc1af2b33158da1
AllysaMaePalawag <allysamaepalawag@gmail.com>
2020-10-09 21:27:19 +0800

    eloquent model and database seeding

--- app/Borrower.php
+++ app/Borrower.php
@@ -10,14 +10,16 @@ class Borrower extends Model
     protected $guarded = [];
     public $timestamps = false;
 
+    //user
     public function user(){
         return $this->belongsTo('App\User', 'user_id');
     }
 
+    //book borrowed
     public function borrowed(){
         return $this->hasMany('App\BorrowedBook', 'borrower_id');
     }
-
+    //book reserved
     public function reservations(){
         return $this->hasMany('App\BookReservation', 'borrower_id');
     }
