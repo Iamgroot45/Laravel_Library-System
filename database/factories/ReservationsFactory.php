810a1778897e4a107e6c6dcf5553aedfce3b268e
Jhonnie Palaganas - Designer, Programmer, Photographer <jhonnie.savior@gmail.com>
2020-10-13 01:20:42 +0800

    added TDD

--- database/factories/ReservationsFactory.php
+++ database/factories/ReservationsFactory.php
@@ -2,14 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\BookReservation;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(BookReservation::class, function (Faker $faker) {
     return [
         'book_id'=>$faker->book_id,
         'reservation_date'=>$faker->reservations_date,
         'pickup_date'=>$faker->pickup_date
     ];
-
 });
